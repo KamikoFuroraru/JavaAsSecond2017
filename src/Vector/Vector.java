670af0cd7ccc61584876ae98d8bb3d0efc2082af
KamikoFuroraru <veramagical@mail.ru>
2017-02-14 18:42:01 +0400

    a wee bit

--- src/Vector/Vector.java
+++ src/Vector/Vector.java
@@ -1,7 +1,51 @@
 package Vector;
 
-public class Vector {
-    public int test(int a, int b) {
-        return a + b;
+public final class Vector {
+
+    private final int x;                       //здесь точно будет int?
+    private final int y;
+    private final int z;
+
+    public Vector(int x, int y, int z) {        //по-твоему это всё??
+        this.x = x;
+        this.y = y;
+        this.z = z;
     }
+
+    public Vector sum(Vector other) {
+        return new Vector(
+                x + other.x,
+                y + other.y,
+                z + other.z
+        );
+    }
+
+    public Vector difference(Vector other) {
+        return new Vector(
+                x - other.x,
+                y - other.y,
+                z - other.z
+        );
+    }
+
+    public Vector multiplication(int k) {
+        return new Vector(
+                k*x,
+                k*y,
+                k*z
+        );
+    }
+
+    public Vector division(int k) {
+        return new Vector(
+                x/k,                             //может получиться не int, подумать
+                y/k,
+                z/k
+        );
+    }
+
+    public Vector scalarProduct(Vector other, ) {
+
+    }
+
 }
